29bd78f8b3c9235cb99d50d270aaf2a8208e1a01
William Junior <wfribeiro1@gmail.com>
2022-11-01 15:50:05 -0300

    Adicionamos a pagina de artilheiros

--- src/app/artilharia/artilharia.page.ts
+++ src/app/artilharia/artilharia.page.ts
@@ -1,3 +1,4 @@
+import { InfoArtilharia } from './../models/InfoArtilharia.models';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artilharia.page.scss'],
 })
 export class ArtilhariaPage implements OnInit {
+
+titulo= 'teste';
+
+  artilheiros: InfoArtilharia[] = [
+    {
+      nome: 'William JR',
+      gols: 19,
+    },
+    {
+      nome: 'Rodrigo Furts',
+      gols: 15,
+    },
+    {
+      nome: 'Luiz Felipe',
+      gols: 11,
+    },
+    {
+      nome: 'Matheus Reis',
+      gols: 8,
+    },
+    {
+      nome: 'Caio Ribeiro',
+      gols: 6,
+    },
+    {
+      nome: 'Fernando Merched',
+      gols: 5,
+    },
+    {
+      nome: 'Lorenzo Gonzada',
+      gols: 4,
+    },
+    {
+      nome: 'Felipe Lima',
+      gols: 3,
+    },
+    {
+      nome: 'Gustavo Ribeiro',
+      gols: 1,
+    }
+
+  ];
 
   constructor() { }
 
---
+++ src/app/models/InfoArtilharia.models.ts
@@ -0,0 +1,4 @@
+export interface InfoArtilharia {
+  nome: string;
+  gols: number;
+}
